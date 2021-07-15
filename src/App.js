87835832d84edd201a52b61fdf25bf3a0af6b797
daryllast <daryl.last621@gmail.com>
2021-07-15 11:56:40 -0400

    sequelize sync

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 import './App.css';
+import { DatePicker } from 'antd';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+        <div className={'flex justify-center p-6 border-b-4 border-gray-300'}>
+            <h1 className={'text-4xl uppercase font-bold tracking-wider'}>Capstone App</h1>
+
+        </div>
+
+        <div className={'grid grid-cols-12'}>
+           <div className={'col-span-12 md:col-span-2 h-96 bg-gray-200'}>
+              <h1>Category Listing</h1>
+
+                </div>
+
+                <div className={'col-span-12 md:col-span-10 h-96 bg-gray-300'}>
+                <h1>Question/Answer Listing</h1>
+                </div>
+          </div>
+
+    </>
   );
 }
 
