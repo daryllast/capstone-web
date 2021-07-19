d5552b173d1f37b3dd7a11374815858ac7377f72
daryllast <daryl.last621@gmail.com>
2021-07-19 14:19:46 -0400

    questions and categories

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 import './App.css';
 import { DatePicker, Button } from 'antd';
 import {useEffect, useState} from 'react'
+import { Modal, Collapse } from 'antd';
+import { isCompositeComponent } from 'react-dom/test-utils';
+const { Panel } = Collapse;
+
+// import ListBody from 'antd/lib/transfer/ListBody';
 
 function App() {
 
     const [categories, setCategories] = useState();
     const [selectedCategory, setSelectedCategory] = useState();
+    const [selectedQuestion, setSelectedQuestion] = useState();
     const [questions, setQuestions] = useState();
 
+    const [showQuestionForm, setShowQuestionForm] = useState(false);
+  
+
+    const [questionTxt, setQuestionTxt] = useState();
 
     const fetchCategories = async () => {
         let res = await fetch('http://localhost:3000/api/v1/categories')
@@ -41,6 +51,42 @@ function App() {
 
     };
 
+const createQuestion = async () => {
+console.log('questionTxt', questionTxt);
+console.log('selectedCategory', selectedCategory);
+let res = await fetch(`http://localhost:3000/api/v1/categories/${selectedCategory.id}/questions`, {
+  method: 'POST',
+  headers: {
+  'Content-Type': 'application/json'
+},
+
+body: JSON.stringify({questionTxt: questionTxt})
+
+})
+
+let data = await res.json()
+console.log(data)
+fetchQuestions(selectedCategory);
+setQuestionTxt('');
+setShowQuestionForm(false);
+
+    };
+
+    const onPanelChange = async (questionId) => {
+      console.log(questionId)
+      let q
+      questions.map((question) => {
+        if (question.id == questionId) {
+          q = question
+        }
+      })
+      console.log(q)
+      setSelectedQuestion(q)
+      console.log('panel was clicked')
+    };
+
+  
+
   return (
     <>
 
@@ -48,6 +94,24 @@ function App() {
             <h1 className={'text-4xl uppercase font-bold tracking-wider'}>Capstone App</h1>
 
         </div>
+
+      
+
+        <Modal title="New Question" visible={showQuestionForm} closable={true} maskClosable={true} footer={null}>
+        {selectedCategory && <div className={'w-full p-2'}>
+                <textarea value={questionTxt} 
+                onChange ={(ev)=> setQuestionTxt(ev.currentTarget.value)} 
+                type="text" 
+                rows={4}
+                className={'border p-1 w-full mb-4'} 
+                placeholder={'Enter the question text...'} />
+
+                <button className={'px-4 py-3 bg-blue-500 text-white rounded mr-4'} onClick={createQuestion}>Create Question</button>
+                <button className={'px-4 py-3 bg-red-500 text-white rounded'} onClick={() =>setShowQuestionForm(false)}>Cancel</button>
+                </div>}
+      </Modal>
+
+     
 
         <div className={'grid grid-cols-12'}>
             <div className={'col-span-12 md:col-span-2'}>
@@ -57,7 +121,7 @@ function App() {
                     {categories && categories.map((category) => {
                         return <li key={category.id}
                                    onClick={() => switchCategory(category)}
-                                   className={(selectedCategory && (selectedCategory.id == category.id)) ?  'p-14 border-b text-3xl bg-gray-200' : 'p-14 border-b text-3xl'}>{category.name}</li>
+                                   className={(selectedCategory && (selectedCategory.id == category.id)) ?  'p-14 border-b text-3xl bg-gray-200 cursor-pointer' : 'p-14 border-b text-3xl cursor-pointer'}>{category.name}</li>
                     })}
                 </ul>
 
@@ -67,19 +131,44 @@ function App() {
 
             </div>
 
-            <div className={'col-span-12 border md:col-span-10 h-96 bg-gray-300'}>
-                <h1>Question/Answer Listing</h1>
+            <div className={'col-span-12 border md:col-span-10'}>
+                
 
-                <hr/>
+               {/* { selectedCategory && <div className={'w-1/3 p-2'}>
+                <textarea value={questionTxt} 
+                onChange ={(ev)=> setQuestionTxt(ev.currentTarget.value)} 
+                type="text" 
+                className={'border p-1 w-full'} 
+                placeholder={'Enter the question text...'} />
 
-                <ul>
-                {questions && questions.map((question) =>  {
-                  return <li key={question.id}>
-                   {question.questionTxt}
-                  </li>
-                })}
-</ul>
+                <button className={'px-4 py-3 bg-blue-500 text-white rouned'} onClick={createQuestion}>Create Question</button>
 
+              
+                </div>} */}
+                {selectedCategory && <div className={'p-4'}>
+                <button className={'px-4 py-3 bg-blue-500 text-white rouned'} onClick={() => setShowQuestionForm(true)}>New Question</button>
+                </div>}
+                
+
+                
+                {selectedCategory ? <h1 className={'text-center text-4xl uppercase'}>Questions</h1> : <h1 className={'text-center text-4xl mt-20 uppercase text-blue-500 cursor-pointer'}>Select a Category to Continue</h1>}
+
+
+                {selectedCategory && questions && questions.length>0 && <div className={'flex justify-center px-24 w-full'}>
+                <Collapse accordion className={'w-full '} onChange={onPanelChange}>
+                {questions && questions.map((question) =>{
+
+                
+    return <Panel header={question.questionTxt} key={question.id}>
+      <p>This is where you add the answers list for this question</p>
+
+      <button className={'px-2 py-1 bg-blue-500 text-white rouned'}>New Answer</button>
+    </Panel> })}
+
+  </Collapse>
+
+        
+</div>}
             </div>
 
         </div>
